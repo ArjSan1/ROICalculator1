c348c8e2873ce9b8e05a0c4066ccdcfd1d1e6a37
ArjSan1 <66225751+ArjSan1@users.noreply.github.com>
2020-06-06 11:36:21 -0500

    Change Start Date to default to Today's date

--- src/ROICalculator.java
+++ src/ROICalculator.java
@@ -1,23 +1,27 @@
 import java.text.DecimalFormat;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
+
+import static java.time.Duration.*;
 
 public class ROICalculator{
 
     public static void main(String[] args) {
     System.out.println("Your initial amount is: "+args[0]);
     System.out.println("Your final amount is: " + args[1]);
-    System.out.println("Start Date: " +args[2]);
-    System.out.println("Expiration date: " + args[3]);
+    System.out.println("Expiration today: " + args[2]);
     double initialAmount = Double.parseDouble(args[0]);
     double finalAmount = Double.parseDouble(args[1]);
-    LocalDate d1 = LocalDate.parse(args[2], DateTimeFormatter.ISO_LOCAL_DATE);
-    LocalDate d2 = LocalDate.parse(args[3], DateTimeFormatter.ISO_LOCAL_DATE);
-    Duration days = Duration.between(d1.atStartOfDay(), d2.atStartOfDay());
+    Date input = new Date();
+    LocalDate today = input.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+    System.out.println("Start Date: " + today);
+    LocalDate d2 = LocalDate.parse(args[2], DateTimeFormatter.ISO_LOCAL_DATE);
+    Duration days = between(today.atStartOfDay(), d2.atStartOfDay());
     double daysBetween = days.toDays();
-    System.out.println("The amount of days between " + args[2] + " and " + args[3]+ " is " + daysBetween + " days.");
+    System.out.println("The amount of days between " + today + " and " + args[2]+ " is " + daysBetween + " days.");
     double numOfYears = 365/daysBetween;
     double  d = finalAmount/ initialAmount;
     double AnnualizedRateOfReturn = ((Math.pow(d,numOfYears)-1)*100);
